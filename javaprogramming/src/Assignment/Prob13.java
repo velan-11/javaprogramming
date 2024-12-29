84c5e544130e981467a6e34d9f5ac6a57907afca
velan-11 <chick11112004@gmail.com>
2024-12-29 18:08:56 +0530

    inital Commit.

--- javaprogramming/src/Assignment/Prob13.java
+++ javaprogramming/src/Assignment/Prob13.java
@@ -5,15 +5,41 @@ import java.util.Scanner;
 public class Prob13 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Gender :");
-		String s = sc.nextLine();
-		char gender = s.charAt(0);
-		System.out.println("Enter your year of service: ");
+		System.out.print("Enter the Gender (male or female) :");
+		String gender = sc.nextLine();
+		System.out.print("Enter your year of service: ");
 		int service=sc.nextInt();
-		System.out.println("Enter the qualification :");
+		sc.nextLine();//using this because the nextInt() takes only the digits and leave the space that appears after enter
+		//so the next nextLine() function takes the blank line as input.To avoid this use nextLine() function after every nextInt()
+		System.out.print("Enter the qualification(grad or postgrad) :");
 		String qualification = sc.nextLine();
+		sc.close();
 		
 		salary(gender,service,qualification);
 	}
-	public static void salary()
+
+	public static void salary(String gender,int service,String qual) {
+		if(gender.equals("male") ) {
+			if(service >=10 && qual.equals("postgrad")) {
+				System.out.println("Your salary is Rs.15,000");
+			}else if(service >=10 && qual.equals("grad")) {
+				System.out.println("Your salary is Rs.10,000");
+			}else if(service <10 && qual.equals("postgrad")) {
+				System.out.println("Your salary is Rs.10,000");
+			}else if(service <10 && qual.equals("grad")) {
+				System.out.println("Your salary is Rs.7,000");
+		}
+	}else if(gender.equals("female")) {
+		if(service >=10 && qual.equals("postgrad")) {
+			System.out.println("Your salary is Rs.12,000");
+		}else if(service >=10 && qual.equals("grad")) {
+			System.out.println("Your salary is Rs.9,000");
+			
+		}
+		
+		
+	}else {
+		System.out.println("Enter the valid gender");
+	}
+	}
 }
